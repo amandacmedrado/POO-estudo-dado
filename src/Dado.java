dc77408a623ac8a831796490cc52698076f358df
Amanda Medrado <amandacmedrado71@gmail.com>
2023-09-24 14:55:32 -0300

    Alterando o uso do construtor

--- src/Dado.java
+++ src/Dado.java
@@ -34,7 +34,7 @@ public class Dado {
 
     public static class Main {
         public static void main(String[] args) {
-            Dado d1 = new Dado();
+            Dado d1 = new Dado(5);
             System.out.println(d1.rolar());
             System.out.println(d1.rolar());
 
